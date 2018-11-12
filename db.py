fb1dd88c41b5115ee86bc739f63143ac571714dc
Denis Koshelev <koden1998@mail.ru>
2018-11-12 22:31:16 +0300

    fixed

--- db.py
+++ db.py
@@ -4,9 +4,11 @@ import sys
 import time
 import logging
 
+
 def csv_reader(file_obj):
     reader = csv.reader(file_obj)
     return reader
+
 
 def write_to_database(file):
     global note_number
@@ -14,7 +16,7 @@ def write_to_database(file):
     conn = sqlite3.connect("data_base.sqlite3")
     cursor = conn.cursor()
     cursor.execute("SELECT sql FROM sqlite_master WHERE name='calls'")
-    if not cursor.fetchall(): #  проверяем, есть ли таблица calls в данной базе данных, если нет - создём её
+    if not cursor.fetchall():  # проверяем, есть ли таблица calls в данной базе данных, если нет - создём её
         cursor.execute("""CREATE TABLE calls
                           ('Crime Id' integer UNIQUE, 'Original Crime Type Name' text, 'Report Date' text, 
                             'Call Date' text,'Offense Date' text, 'Call Time' text, 'Call Date Time' text, 'Disposition' text, 
@@ -22,31 +24,20 @@ def write_to_database(file):
                             'Common Location' text
                           )
                        """)
-
-    note_number = -1
-    error = 0
-    for row in file:
-        print(row) # "Скрипт должен отображать процесс загрузки" , замедляет время его работы примерное в 2 раза
-        if note_number == -1: # пропускаем первую строку, в который содержатся названия колонок
-            note_number = 0
-            continue
-        new_row = []
-        for column in row: # иногда, в строках встречаются двойные кавычки(обычно в каких-либо названиях)
-            column = column.replace('"', "'") # для того, что бы избежать конфликтов с бд, меняем двойные кавычки на одинарные
-            new_row.append(f'"{column}"')
-        insert = f'INSERT INTO calls VALUES({", ".join(new_row)})'
-        try:
-            cursor.execute(insert)
-            note_number += 1
-        except(sqlite3.OperationalError):
-            error += 1
+    print("Считывания данных из csv файла...")
+    calls = list(file)
+    note_number = len(calls)
+    print("Запись в базу данных...")
+    cursor.executemany("INSERT INTO calls VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)", calls)
     conn.commit()
 
+
 def write_log():
+    print(f"Записей в базе {note_number}")
+    print(f"Время выполнения {round(time.process_time(), 2)}")
     logging.info("Запись окончена:\n"
             f"{note_number} записей в базе данных\n"
-            f"{error} ошибок\n"
-            f"{round(time.process_time(),2)} время выполнения")
+            f"{round(time.process_time(), 2)} время выполнения")
 
 
 if __name__ == "__main__":
